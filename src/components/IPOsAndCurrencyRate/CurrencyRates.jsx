64f13ffdeff456998e7105071e49f3ec803c6e37
akhilesh-sahu12 <akhilesh.jamus@gmail.com>
2024-01-07 17:51:03 +0530

    add refresh button

--- src/components/IPOsAndCurrencyRate/CurrencyRates.jsx
+++ src/components/IPOsAndCurrencyRate/CurrencyRates.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useCurrencyRatesData from '../../hooks/useCurrencyRatesData';
 
 const CurrencyRates = () => {
-  const currencyRates = useCurrencyRatesData();
+  const [refresh, setRefresh] = useState(false);
+  const currencyRates = useCurrencyRatesData(refresh);
+
+  const handleRefresh = () => {
+    setRefresh(!refresh);
+  };
 
   return (
     <div className="currency-rates-container">
       <h2>Latest Currency Rates</h2>
+      <button onClick={handleRefresh}>Refresh</button>
       <ul>
-        {currencyRates && currencyRates.map((rate) => (
-          <li key={rate.symbol}>
-            <strong>{rate.symbol}:</strong> {rate.rate}
+        {currencyRates && currencyRates.map((currencyRate) => (
+          <li key={currencyRate.symbol}>
+            <li><strong>Date:</strong> {new Date(currencyRate.timestamp).toLocaleString()}</li>
+            <li><strong>{currencyRate.symbol}:</strong> {currencyRate.rate}</li>
           </li>
         ))}
       </ul>
--- src/components/IPOsAndCurrencyRate/UpcomingIPOs.jsx
+++ src/components/IPOsAndCurrencyRate/UpcomingIPOs.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import useIPOsData from '../../hooks/useIPOsData'; 
 
 const UpcomingIPOs = () => {
-    const upcomingIPOs = useIPOsData(); // Fetching IPOs data using the hook
+    const [refresh, setRefresh] = useState(false);
+    const upcomingIPOs = useIPOsData(refresh); // Fetching IPOs data using the hook
+
+    const handleRefresh = () => {
+      setRefresh(!refresh);
+    };
 
     return (
       <div className="upcoming-ipos-container">
         <h2>Upcoming IPOs</h2>
-
+        <button onClick={handleRefresh} className="refresh-button">Refresh</button>
         <div className="ipo-cards">
           {upcomingIPOs.map((ipo, index) => (
             <div key={index} className="ipo-card">
               <h3>{ipo.companyName}</h3>
               <p>Symbol: {ipo.symbol}</p>
               <p>Offering Date: {ipo.offeringDate}</p>
+              <p>Volume: {ipo.volume}</p>
             </div>
           ))}
         </div>
